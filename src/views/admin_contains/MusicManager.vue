24231c6819491dcca12476fc8507300592d43294
Idoyt <2039316792@qq.com>
2024-05-22 23:45:44 +0800

    更新了MusicManager的部分代码，使其更加合理，匹配数据库的字段与后端接口

--- src/views/admin_contains/MusicManager.vue
+++ src/views/admin_contains/MusicManager.vue
@@ -17,43 +17,51 @@ export default defineComponent({
       artist: '',
       genre: '',
       album: '',
-    });
-    const file = reactive({
-      audio: null,
-      lyric: null,
-      cover: null,
+      files: {
+        audio: null,
+        lyric: null,
+        cover: null,
+      },
     });
 
 
     const showDrawer = ()=>{
       newMusicInfoDrawer.value = true;
-      console.log(newMusicInfoDrawer.value);
     };
-    const submit = ()=> {
-      console.log(newMusicInfo.value);
-      console.log(file);
-    };
-
-    const beforeDrawerClose = (done) => {
-      // 清空表单数据
-      musicForm.value.resetFields();
-      done();
-    };
-    const submitForm = ()=> {
+    const submitForm = async ()=> {
       console.log('提交表单:', newMusicInfo);
+      const formData = new FormData();
 
+      for (const key in newMusicInfo) {
+        if (!newMusicInfo.hasOwnProperty(key)) continue;
+        if (key === 'files') {
+          for (const file in newMusicInfo[key]) {
+            if (newMusicInfo[key].hasOwnProperty(file)) {
+              formData.append(file, newMusicInfo[key][file]);
+            }
+          }
+        } else {
+          formData.append(key, newMusicInfo[key]);
+        }
+      }
+
+      const response = await axios.post(API_BASE_URL + '/new_music_info/', formData, {withCredentials: true});
+      console.log(response);
       // 提交完成后关闭 el-drawer
       newMusicInfoDrawer.value = false;
+    };
+    const reshapeFiles = (event, type)=>{
+      newMusicInfo.files[type] = Array.from(event.target.files)[0];
     };
 
 
     onBeforeMount(async ()=>{
       const data = {};
       const response = await axios.get(API_BASE_URL + '/get_music_info/', {params: data, withCredentials: true});
-      console.log(response);
       if (response.data.state === 'success') {
         musicData.value = response.data.message;
         total.value = musicData.value.length;
+        console.log(musicData.value);
       } else alert('Get music info failed, please try again later');
     });
 
@@ -62,14 +70,12 @@ export default defineComponent({
       musicListData,
       newMusicInfoDrawer,
       newMusicInfo,
-      submit,
-      file,
       musicForm,
-      beforeDrawerClose,
       submitForm,
       showDrawer,
       total,
       pageSize,
+      reshapeFiles,
     };
   },
 });
@@ -77,12 +83,11 @@ export default defineComponent({
 
 <template>
   <div id="musicManageBody">
-    <el-button @click="showDrawer">New Music</el-button>
+    <el-button @click="showDrawer">新建音乐信息</el-button>
 
     <el-drawer
         v-model="newMusicInfoDrawer"
-        :before-close="beforeDrawerClose"
-        :width="400"
+        :width="600"
         title="提交音乐信息"
     >
       <!-- el-form 组件，用于输入音乐信息 -->
@@ -100,34 +105,13 @@ export default defineComponent({
           <el-input v-model="newMusicInfo.album"></el-input>
         </el-form-item>
         <el-form-item label="音频文件">
-          <el-upload
-              :before-upload="beforeAudioUpload"
-              action="/upload_audio"
-              class="upload-demo"
-              drag
-          >
-            <el-button size="small" type="primary">点击上传</el-button>
-          </el-upload>
+          <input accept=".ogg,.mp3,.wav" type="file" @change="reshapeFiles($event, 'audio')"/>
         </el-form-item>
         <el-form-item label="音乐封面">
-          <el-upload
-              :before-upload="beforeCoverUpload"
-              action="/upload_cover"
-              class="upload-demo"
-              drag
-          >
-            <el-button size="small" type="primary">点击上传</el-button>
-          </el-upload>
+          <input accept=".png" type="file" @change="reshapeFiles($event, 'cover')"/>
         </el-form-item>
         <el-form-item label="歌词文件">
-          <el-upload
-              :before-upload="beforeLyricsUpload"
-              action="/upload_lyrics"
-              class="upload-demo"
-              drag
-          >
-            <el-button size="small" type="primary">点击上传</el-button>
-          </el-upload>
+          <input accept=".lrc" type="file" @change="reshapeFiles($event, 'lyric')"/>
         </el-form-item>
       </el-form>
 
@@ -137,25 +121,25 @@ export default defineComponent({
 
     <div style="display: flex; flex-direction: column">
       <div style="height: 90vh; overflow: hidden">
-        <el-table :data="musicData" :row-key="row => row.music_id" stripe style="width: 100%">
+        <el-table :data="musicData" :fit="true" :row-key="row => row.music_id" stripe style="width: 100%">
+          <el-table-column label="Music ID" prop="music_id"></el-table-column>
           <el-table-column label="Title" prop="title"></el-table-column>
           <el-table-column label="Artist" prop="artist"></el-table-column>
           <el-table-column label="Genre" prop="genre"></el-table-column>
           <el-table-column label="Album" prop="album"></el-table-column>
           <el-table-column label="Release Date" prop="release_date"></el-table-column>
-          <el-table-column label="Cover" prop="cover_url"></el-table-column>
-          <el-table-column label="Source" prop="source_url"></el-table-column>
-          <el-table-column label="Lyrics" prop="lyrics_url"></el-table-column>
+          <el-table-column label="Cover" prop="cover_extension"></el-table-column>
+          <el-table-column label="Source" prop="source_extension"></el-table-column>
+          <el-table-column label="Lyrics" prop="lyrics_extension"></el-table-column>
           <el-table-column label="Likes" prop="likes"></el-table-column>
           <el-table-column label="Shares" prop="shares"></el-table-column>
           <el-table-column label="Search Volume" prop="search_volume"></el-table-column>
-          <el-table-column label="Extension" prop="extension"></el-table-column>
           <el-table-column label="Active" prop="is_active"></el-table-column>
           <el-table-column label="Reviewed" prop="is_reviewed"></el-table-column>
         </el-table>
       </div>
 
-      <el-pagination :total="1000" layout="prev, pager, next" />
+      <el-pagination :total="total" layout="prev, pager, next" />
     </div>
 
 
